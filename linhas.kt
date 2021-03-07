1c28251cd9fbb440ef093921672211e06f13b5dc
Yudi Aguena <yudi.aguena@gmail.com>
2021-03-06 21:23:19 -0300

    Update linhas.kt

--- linhas.kt
+++ linhas.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>) {
     var r = 0
     for (i in 1..readLine()!!.toInt()) {
 
-        println("$i ${i*i} ${i*i*i}") 
+        println("$i ${ i*i } ${ i*i*i }") 
 
         r += 0
     }
