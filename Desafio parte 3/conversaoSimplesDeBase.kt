368d69053519be87abc69c441ab1cea5bed5b2a3
Yudi Aguena <yudi.aguena@gmail.com>
2021-03-21 00:15:29 -0300

    Update conversaoSimplesDeBase.kt

--- Desafio parte 3/conversaoSimplesDeBase.kt
+++ Desafio parte 3/conversaoSimplesDeBase.kt
@@ -5,8 +5,26 @@ fun main(args: Array<String>) {
 	val r = """0x.*""".toRegex()
 	while(true) {
 		
-//escreva sua solução aqui
-
-		}
+        val v = readLine()!!
+        try {
+            val value = v.toInt()
+            if (value == -1) { break } 
+        } catch (e: Exception) {}
+        if (v.contains(r)) {
+            hexaToDecimal(s) 
+        } else {
+            decimalToHexa(s)
+    	}
 	}
+}
+
+fun decimalToHexa(s: String) {
+    val n = s.toInt()
+    println("0x${Integer.toHexString(n).toUpperCase()}")
+}
+
+fun hexaToDecimal(s: String) {
+    val hex = s.subSequence(2, s.length).toString()
+    val n = Integer.parseInt(hex, 16)
+    println(n)
 }
